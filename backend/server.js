561c7b51b6ec6819ab92aa152021530cada9d060
jayceforte <jayce4tay@gmail.com>
2025-05-18 12:32:26 -0600

    jacking with deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4001;
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
